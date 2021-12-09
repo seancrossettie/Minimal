69f78cd5dd2d96171bae5dd866757ad9ed1e702a
seancrossettie <seancrossettie@gmail.com>
2021-12-09 14:06:33 -0600

    created models

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Minimal.Models
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public Guid UserId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public Int16 TotalCategoryItems { get; set; }
+        public Int16 TotalCategoryItemsRemoved { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
